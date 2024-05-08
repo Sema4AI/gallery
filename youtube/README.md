44b3d56caef4a1c971ce1f520b0f993ec1c78b52
tonnitommi <tommi.holmgren@gmail.com>
2024-05-08 10:20:13 +0300

    First version of youtube actions

---
+++ youtube/README.md
@@ -0,0 +1,14 @@
+# Youtube Actions
+
+Action Package to interact with Youtube.
+
+## Authentication
+
+Uses only publicly available API's no need for API keys.
+
+## Actions
+
+The following actions are contained with this package:
+
+### get_youtube_transcript
+Extracts the transcription of a Youtube video. Works for videos with subtitles or captions.
---
+++ youtube/actions.py
@@ -0,0 +1,35 @@
+"""
+Youtube Actions for Sema4.ai Action Server
+"""
+
+from sema4ai.actions import action
+from youtube_transcript_api import YouTubeTranscriptApi
+
+
+@action(is_consequential=False)
+def get_youtube_transcript(video_url: str) -> str:
+    """
+    Extracts the transcription of a Youtube video. Only works if subtitles or captions are available for the video.
+
+    Args:
+        video_url (str): Youtube video URL as a complete URL, example: "https://www.youtube.com/watch?v=YOUR_VIDEO_ID"
+
+    Returns:
+        str: Complete transcription of the youtube video
+    """
+
+    # Extract video ID from the URL
+    video_id = video_url.split('v=')[1]
+    # Handle video URLs with additional parameters after the video ID
+    ampersand_position = video_id.find('&')
+    if ampersand_position != -1:
+        video_id = video_id[:ampersand_position]
+    
+    try:
+        # Fetching the transcript
+        transcript_list = YouTubeTranscriptApi.get_transcript(video_id)
+        # Concatenate all text parts to create a full transcript
+        transcript = ' '.join([entry['text'] for entry in transcript_list])
+        return transcript
+    except Exception as e:
+        return f"An error occurred: {str(e)}"
