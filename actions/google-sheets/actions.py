da84e258fd7c6f4185470fd94d25c4e04bcb3415
Vlad <vlad@sema4.ai>
2024-05-29 13:21:44 +0300

    Update google sheets to use OAuth2Secret and implement the new Response pattern

--- actions/google-sheets/actions.py
+++ actions/google-sheets/actions.py
@@ -8,23 +8,49 @@ Currently supporting:
 - Add rows to a sheet
 """
 
-import json
-import os
 from pathlib import Path
-from typing import Annotated, List
+from typing import Annotated, List, Literal
 
 import gspread
 from dotenv import load_dotenv
 from gspread import Worksheet
+from gspread.auth import Credentials
+from gspread.exceptions import APIError
 from pydantic import BaseModel, Field
-from sema4ai.actions import Secret, action
+from sema4ai.actions import ActionError, OAuth2Secret, Response, action
+from typing_extensions import Self
 
 load_dotenv(Path(__file__).absolute().parent / "devdata" / ".env")
 
-DEV_GOOGLE_SERVICE_ACCOUNT = Secret.model_validate(
-    os.getenv("DEV_GOOGLE_SERVICE_ACCOUNT", "")
-)
+
 ALPHA_LENGTH = ord("Z") - ord("A") + 1
+
+SCOPES = list[
+    Literal[
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive",
+    ]
+]
+
+
+class _CatchError:
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if isinstance(exc_val, APIError):
+            raise ActionError(str(exc_val)) from None
+
+
+class _Credentials(Credentials):
+    @classmethod
+    def from_oauth2_secret(cls, secret: OAuth2Secret) -> Self:
+        credentials = cls()
+        credentials.token = secret.access_token
+        return credentials
+
+    def refresh(self, request):
+        raise ActionError("OAuth2 token expired")
 
 
 class Row(BaseModel):
@@ -40,32 +66,35 @@ class RowData(BaseModel):
 
 @action(is_consequential=True)
 def create_spreadsheet(
-    name: str, google_credentials: Secret = DEV_GOOGLE_SERVICE_ACCOUNT
-) -> str:
+    oauth_access_token: OAuth2Secret[Literal["google"], SCOPES],
+    name: str,
+) -> Response[str]:
     """Creates a new Spreadsheet.
 
     Args:
+        oauth_access_token: The OAuth2 access token .
         name: Name of the Spreadsheet, which will be later used when reading or writing into it.
-        google_credentials: JSON containing the Google service account credentials.
-
     Returns:
         Message containing the spreadsheet title and url.
     """
-    gc = gspread.service_account_from_dict(json.loads(google_credentials.value))
 
-    spreadsheet = gc.create(name)
+    with _CatchError():
+        gc = gspread.authorize(_Credentials.from_oauth2_secret(oauth_access_token))
+        spreadsheet = gc.create(name)
 
-    return f"Sheet created: {spreadsheet.title}: {spreadsheet.url}"
+        return Response[str](
+            result=f"Sheet created: {spreadsheet.title}: {spreadsheet.url}"
+        )
 
 
 @action(is_consequential=True)
 def create_worksheet(
+    oauth_access_token: OAuth2Secret[Literal["google"], SCOPES],
     spreadsheet: str,
     title: str,
     rows: int = 100,
     columns: int = 20,
-    google_credentials: Secret = DEV_GOOGLE_SERVICE_ACCOUNT,
-) -> str:
+) -> Response[str]:
     """Creates a new Worksheet.
 
     Args:
@@ -73,27 +102,31 @@ def create_worksheet(
         title: The title of the new Worksheet.
         rows: Number of rows to be added in the new Worksheet.
         columns: Number of columns to be added in the new Worksheet.
-        google_credentials: JSON containing the Google service account credentials.
+        oauth_access_token: The OAuth2 access token .
 
     Returns:
         Message containing the newly created worksheet title and url.
     """
-    gc = gspread.service_account_from_dict(json.loads(google_credentials.value))
 
-    spreadsheet = gc.open(spreadsheet)
-    worksheet = spreadsheet.add_worksheet(title=title, rows=rows, cols=columns)
+    with _CatchError():
+        gc = gspread.authorize(_Credentials.from_oauth2_secret(oauth_access_token))
 
-    return f"Worksheet successfully created the worksheet: {worksheet.title}: {worksheet.url}"
+        spreadsheet = gc.open(spreadsheet)
+        worksheet = spreadsheet.add_worksheet(title=title, rows=rows, cols=columns)
+
+        return Response(
+            result=f"Worksheet successfully created the worksheet: {worksheet.title}: {worksheet.url}"
+        )
 
 
 @action(is_consequential=False)
 def get_sheet_content(
+    oauth_access_token: OAuth2Secret[Literal["google"], SCOPES],
     spreadsheet: str,
     worksheet: str,
     from_row: int = 1,
     limit: int = 100,
-    google_credentials: Secret = DEV_GOOGLE_SERVICE_ACCOUNT,
-) -> str:
+) -> Response[str]:
     """Get all content from the chosen Google Spreadsheet Sheet.
 
     To avoid performance issues there is a limit of rows to retrieve starting
@@ -104,54 +137,60 @@ def get_sheet_content(
         worksheet: Name of the worksheet within the spreadsheet.
         from_row: Used for pagination, default is first row.
         limit: How many rows to retrieve starting from line number defined in `from_row`.
-        google_credentials: JSON containing the Google service account credentials.
+        oauth_access_token: The OAuth2 access token .
 
     Returns:
         The sheet's content.
     """
 
-    gc = gspread.service_account_from_dict(json.loads(google_credentials.value))
+    with _CatchError():
+        gc = gspread.authorize(_Credentials.from_oauth2_secret(oauth_access_token))
 
-    spreadsheet = gc.open(spreadsheet)
-    worksheet = spreadsheet.worksheet(worksheet)
+        spreadsheet = gc.open(spreadsheet)
+        worksheet = spreadsheet.worksheet(worksheet)
 
-    return _get_sheet_content(worksheet, from_row, limit)
+        return Response(result=_get_sheet_content(worksheet, from_row, limit))
 
 
 @action(is_consequential=False)
 def get_spreadsheet_schema(
-    spreadsheet: str, google_credentials: Secret = DEV_GOOGLE_SERVICE_ACCOUNT
-) -> str:
+    oauth_access_token: OAuth2Secret[Literal["google"], SCOPES],
+    spreadsheet: str,
+) -> Response[str]:
     """Get necessary information to be able to work with a Google Spreadsheets correctly.
 
     Method will return the first few rows of each Sheet as an example.
 
     Args:
         spreadsheet: Name of the spreadsheet from which to get the data.
-        google_credentials: JSON containing the Google service account credentials.
+        oauth_access_token: The OAuth2 access token .
 
     Returns:
         Names of the sheets, and the first rows from each sheet to explain the context.
     """
-    gc = gspread.service_account_from_dict(json.loads(google_credentials.value))
-    sh = gc.open(spreadsheet)
 
-    output = "Here are the sheets and their first rows.\n\n"
-    content = []
+    with _CatchError():
+        gc = gspread.authorize(_Credentials.from_oauth2_secret(oauth_access_token))
+        sh = gc.open(spreadsheet)
 
-    for sheet in sh.worksheets():
-        content.append(_get_sheet_content(sheet, from_row=1, limit=5))
+        output = "Here are the sheets and their first rows.\n\n"
+        content = []
 
-    return output + "\n".join(content)
+        for sheet in sh.worksheets():
+            content.append(_get_sheet_content(sheet, from_row=1, limit=5))
+
+        result = f"output + {"\n".join(content)}"
+
+        return Response(result=result)
 
 
 @action(is_consequential=True)
 def add_sheet_rows(
+    oauth_access_token: OAuth2Secret[Literal["google"], SCOPES],
     spreadsheet: str,
     worksheet: str,
     rows_to_add: RowData,
-    google_credentials: Secret = DEV_GOOGLE_SERVICE_ACCOUNT,
-) -> str:
+) -> Response[str]:
     """Add multiple rows to the Google sheet.
 
     Make sure the values are in correct columns (needs to be ordered the same as in the document).
@@ -160,30 +199,31 @@ def add_sheet_rows(
         spreadsheet: Name of the spreadsheet you want to work on.
         worksheet: Name of the sheet where the data is added to.
         rows_to_add: The rows to be added to the end of the sheet.
-        google_credentials: JSON containing the Google service account credentials.
+        oauth_access_token: The OAuth2 access token .
 
     Returns:
         Message indicating the success of the operation.
     """
 
-    gc = gspread.service_account_from_dict(json.loads(google_credentials.value))
+    with _CatchError():
+        gc = gspread.authorize(_Credentials.from_oauth2_secret(oauth_access_token))
 
-    spreadsheet = gc.open(spreadsheet)
-    worksheet = spreadsheet.worksheet(worksheet)
+        spreadsheet = gc.open(spreadsheet)
+        worksheet = spreadsheet.worksheet(worksheet)
 
-    worksheet.append_rows(values=rows_to_add.to_raw_data())
+        worksheet.append_rows(values=rows_to_add.to_raw_data())
 
-    return "Row(s) were successfully added."
+        return Response(result="Row(s) were successfully added.")
 
 
 @action(is_consequential=True)
 def update_sheet_rows(
+    oauth_access_token: OAuth2Secret[Literal["google"], SCOPES],
     spreadsheet: str,
     worksheet: str,
     cells: str,
     data: RowData,
-    google_credentials: Secret = DEV_GOOGLE_SERVICE_ACCOUNT,
-) -> str:
+) -> Response[str]:
     """Update a cell or a range of cells in a worksheet using A1 or R1:C1 notation.
 
     Args:
@@ -191,7 +231,7 @@ def update_sheet_rows(
         worksheet: Name of the sheet where the data is added to.
         cells: Cell or range of cells to update.
         data: Data to be inserted into the cell or cells.
-        google_credentials: JSON containing the Google service account credentials.
+        oauth_access_token: The OAuth2 access token .
 
     Example:
         ```python
@@ -204,14 +244,15 @@ def update_sheet_rows(
          Message indicating the success or failure of the operation.
     """
 
-    gc = gspread.service_account_from_dict(json.loads(google_credentials.value))
+    with _CatchError():
+        gc = gspread.authorize(_Credentials.from_oauth2_secret(oauth_access_token))
 
-    spreadsheet = gc.open(spreadsheet)
-    worksheet = spreadsheet.worksheet(worksheet)
+        spreadsheet = gc.open(spreadsheet)
+        worksheet = spreadsheet.worksheet(worksheet)
 
-    worksheet.update(data.to_raw_data(), range_name=cells)
+        worksheet.update(data.to_raw_data(), range_name=cells)
 
-    return "Rows were successfully updated."
+        return Response(result="Rows were successfully updated.")
 
 
 def _get_sheet_content(worksheet: Worksheet, from_row, limit) -> str:
