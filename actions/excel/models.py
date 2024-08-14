61675d7c3191dec054c9d7cb64df9620721ecb53
Ovidiu Rusu <ovidiu@sema4.ai>
2024-08-14 16:26:04 +0300

    Fix and improve excel operations

--- actions/excel/models.py
+++ actions/excel/models.py
@@ -7,8 +7,6 @@ class Row(BaseModel):
     cells: Annotated[list[str], Field(description="Row cells")]
 
     def __init__(self, cells: list[str]):
-        # NOTE(cmin764): In Sema4.ai Desktop the header is often send as a primitive
-        #  list, while in ReMark it fully understands the expected structure.
         super().__init__(cells=[str(cell) for cell in cells])
 
     def as_list(self) -> list[str]:
@@ -16,11 +14,7 @@ class Row(BaseModel):
 
 
 class Table(BaseModel):
-    header: Annotated[Row | None, Field(None, description="Optional table header")]
-    rows: Annotated[list[Row], Field(description="Table rows")]
-
-    def get_header(self) -> list[str]:
-        return self.header.as_list() if self.header else []
+    rows: Annotated[list[Row], Field(description="The rows that need to be added")]
 
     def as_list(self) -> list[list[str]]:
         return [row.as_list() for row in self.rows]
